Mellanox/virtio-emulation

commit 6bf7fd642b5b52ddb81c744759f3c23f7c6c2a71

net/mlx5_vdpa: Do not fail dev_conf when CREATE_RQ fails
For now FW doesn't support the CREATE_RQ with CQN=0.
For now continue without the CREATE_RQ actually pass.

Signed-off-by: Ido Shamay <idos@mellanox.com>

drivers/net/mlx5/mlx5_vdpa.c
@@ -113,7 +113,8 @@ static int mlx5_vdpa_setup_rx(struct vdpa_priv *priv)
             rte_vhost_get_vhost_vring(priv->vid, i, &vq);
             if (create_rq(priv, vq.size, i)) {
                 DRV_LOG(ERR, "Create RQ failed for Virtqueue %d", i);
-                return -1;
+                /* TODO(idos): Remove this when FW supports */
+                DRV_LOG(INFO, "Contiuing without RQ for Virtqueue %d", i);
             }
         }
     }

commit bbe98e872173bed62a810258088b4afe8daa59a1

net/mlx5_vdpa: fix rxq cleanup on device close
If the RQ is not created don't attempt to free it.

Fixes: 6bf7fd642b5b ("net/mlx5_vdpa: Do not fail dev_conf when CREATE_RQ fails")

Signed-off-by: Shahaf Shuler <shahafs@mellanox.com>

drivers/net/mlx5/mlx5_vdpa.c
@@ -152,6 +152,8 @@ static int mlx5_vdpa_release_rx(struct vdpa_priv *priv)
 	for (i = 0; i < priv->nr_vring; i++) {
 		if (i == 0 || i % 2 == 0) {
 			rq = priv->virtq[i].rq_obj;
+			if (!rq)
+				continue;
 			if (mlx5_glue->dv_devx_obj_destroy(rq)) {
 				DRV_LOG(ERR, "Error DESTROY)RQ VirtQ %d", i);
 				return -1;